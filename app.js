53cc37d5dd209616762b7981d55c6946aa269ab1
Himanshu <himrd95@gmail.com>
2021-06-19 21:55:56 +0530

    added a few images

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .to("bg", 10, { y: -300 })
-  .to(".girl", 10, { y: -200 }, "-=10")
-  .fromTo("bike", { y: -250 }, { y: 0, duration: 10 }, "-=10")
+  
+  .to(".bg", 10, { y: -300 })
+  .to(".girl", 10, { x: -200 }, "-=10")
+  .fromTo(".bike", { y: -200 }, { y: 0, duration: 10 }, "-=10")
   .to(".content", 10, { top: "0%" }, "-=10")
-  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
-  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+  .fromTo(".nav", { opacity: 1 }, { opacity: 0, duration: 5 })
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 5 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 5 });
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
